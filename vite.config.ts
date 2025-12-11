39fecb64596bc8ee9a06b4952ac59203860ad973
oneieo <sun020716@gmail.com>
2025-12-11 18:11:18 +0900

    fix: baseUrl 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Demo_project/",
+  base: "/Demo_Project/",
 });
